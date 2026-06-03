83782f3a721efd6f41fa2c7e1f274080123e1147
Jeremy <79330287+jeremyevith@users.noreply.github.com>
2026-06-03 20:30:00 +0800

    docs: refresh README (drop architecture wording), add OIDC publish workflow, bump 1.2.3 (#49)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.2.2'
+VERSION = '1.2.3'
 DESCRIPTION = 'ID Analyzer API client library, scan and verify global passport, driver license and identification card.'
 
 
